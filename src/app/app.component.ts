71958c5506ff8a79d49baa3b7c988ab8792e6336
Lucas <lucaslando1@gmail.com>
2017-09-11 00:06:29 -0300

    Sharing project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'app';
+  private apiUrl = 'http://localhost:8080/subjects';
+  subjects: any = [];
+
+  constructor(private http: Http) {
+    this.getSubjects();
+  }
+
+  getData() {
+    return this.http.get(this.apiUrl).map((res: Response) => res.json() );
+  }
+
+  getSubjects() {
+    this.getData().subscribe(data => {
+        console.log(data);
+        this.subjects = data;
+    });
+  }
+
 }
