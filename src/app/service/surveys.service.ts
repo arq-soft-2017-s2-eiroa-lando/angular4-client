927df87af5b285b82ea33dbf842978f580e97b36
Lucas Lando <lucaslando1@gmail.com>
2017-12-10 20:01:03 -0300

    Agregando url de backend

--- src/app/service/surveys.service.ts
+++ src/app/service/surveys.service.ts
@@ -1,128 +1,42 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject, OnInit } from '@angular/core';
 import { Headers, Http } from '@angular/http';
-
 import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class SurveyService{
 
   private headers = new Headers({'Content-Type': 'application/json'});
-  private baseUrl = "http://localhost:8080/";
-  private surveyStatistics = {
-    period : "1er cuatrimestre 2018",
-    totalSurveys : "734",
-    surveysCompleted : "482",
-    classes : [
-      {
-        subject : "Organizacion de computadoras",
-        name: "C1",
-        enrolled : "45",
-        schedule: [
-            "Martes 20:00 a 22:00 hs",
-            "Jueves 20:00 a 22:00 hs",
-            "Viernes 09:00 a 12:00 hs"
-        ],
-        size: 60
-      },
-      {
-        subject : "Organizacion de computadoras",
-        name: "C2",
-        enrolled : "4",
-        schedule: [
-            "Martes 08:00 a 10:00 hs",
-            "Jueves 08:00 a 10:00 hs"
-        ],
-        size: 30
-      },
-      {
-        subject : "Introduccion a la programacion",
-        name: "C1",
-        enrolled : "1",
-        schedule: [
-            "Martes 20:00 a 22:00 hs",
-            "Jueves 20:00 a 22:00 hs"
-        ],
-        size: 15
-      },
-      {
-        subject : "Introduccion a la programacion",
-        name: "C2",
-        enrolled : "90",
-        schedule: [
-            "Martes 20:00 a 22:00 hs",
-            "Jueves 20:00 a 22:00 hs"
-        ],
-        size: 100
-      },
-      {
-        subject : "Introduccion a la programacion",
-        name: "C3",
-        enrolled : "55",
-        schedule: [
-            "Martes 20:00 a 22:00 hs",
-            "Jueves 20:00 a 22:00 hs"
-        ],
-        size: 50
-      },
-      {
-        subject : "Objetos 1",
-        name: "C1",
-        enrolled : "10",
-        schedule: [
-            "Martes 20:00 a 22:00 hs",
-            "Jueves 20:00 a 22:00 hs"
-        ],
-        size: 40
-      },
-      {
-        subject : "Objetos 1",
-        name: "C2",
-        enrolled : "100",
-        schedule: [
-            "Martes 20:00 a 22:00 hs",
-            "Jueves 20:00 a 22:00 hs"
-        ],
-        size: 60
-      },
-      {
-        subject : "Objetos 1",
-        name: "C3",
-        enrolled : "0",
-        schedule: [
-            "Martes 20:00 a 22:00 hs",
-            "Jueves 20:00 a 22:00 hs"
-        ],
-        size: 60
-      }
-
-    ]
-
-  }
-
 
   constructor(private http: Http) { }
 
   getSurveyStatistics() : any{
-    return this.http.get(this.baseUrl + "api/survey/statistics")
+    return this.http.get(this.baseUrl() + "api/survey/statistics")
       .toPromise()
       .then(response => response.json() )
       .catch( this.handleError );
   }
 
+  baseUrl(): string {
+    if( document.location.href.indexOf("localhost") == -1 ){
+      return "http://52.11.222.208:8090";
+    }
+    return "http://localhost:8080/";
+  }
+
   saveSurvey(survey): Promise<void>{
-    return this.http.post(this.baseUrl + "api/survey/new-survey", JSON.stringify(survey), {headers: this.headers})
+    return this.http.post(this.baseUrl() + "api/survey/new-survey", JSON.stringify(survey), {headers: this.headers})
       .toPromise().then(() => null )
       .catch(this.handleError);
   }
 
   saveAnswer(hash, answers) : Promise<void>{
-    return this.http.post(this.baseUrl + "api/survey/answer/" + hash, answers, {headers: this.headers})
+    return this.http.post(this.baseUrl() + "api/survey/answer/" + hash, answers, {headers: this.headers})
       .toPromise().then(() => null )
       .catch(this.handleError);
   }
 
   getSurvey(surveyHash): Promise<any>{
-    return this.http.get(this.baseUrl + "api/survey/student-survey/" + surveyHash)
+    return this.http.get(this.baseUrl() + "api/survey/student-survey/" + surveyHash)
                .toPromise()
                .then(response => response.json() )
                .catch(this.handleError);
